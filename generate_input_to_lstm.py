7eab99da137ada74caf40e2064c97765dcc07d45
Gelei Chen <gelei@aisense.com>
2017-10-30 19:42:14 -0400

    check in 5 20 sec avi files

--- generate_input_to_lstm.py
+++ generate_input_to_lstm.py
@@ -6,4 +6,6 @@ wav_file = "../TownCentreXVID.avi"
 def generate_small_batch(start,duration):
     os.system("ffmpeg -i {} -ss {} -t {} -async 1 -c copy videos/{}.avi".format(wav_file,str(start),str(duration),str(start)+"_"+str(duration+start)))
 
-# generate_small_batch(10,10)
+duration = 20
+for i in range(0,100,20):
+    generate_small_batch(i,duration)
